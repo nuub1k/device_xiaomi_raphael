Havoc-Devices/android_device_xiaomi_raphael_old

raphael: Build havoc official

system_prop.mk
@@ -159,3 +159,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Vendor
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.qti.va_aosp.support=1
+
+# Build Official
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.havoc.maintainer=WajidKhan

nuub1k/device_xiaomi_raphael

system_prop.mk
@@ -102,3 +102,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Vendor
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.qti.va_aosp.support=1
+
+# Build Official
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.havoc.maintainer=nuub1k